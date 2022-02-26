27d18b6b8a68055888db23d28469427e93bab810
Prabhat Ranjan <Prabhat0206@hotmail.com>
2022-02-26 12:32:30 +0530

    permussion

--- vendor/views.py
+++ vendor/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
 from .serializer import *
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
 from rest_framework.response import Response
 from django.contrib.auth.models import Group
